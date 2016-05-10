31faedd4e3dbdc1dea0b61a60be278e810b5aca1
Tomas Navarrete <tomas.navarrete@list.lu>
2016-05-10 14:33:58 +0200

    Moves to install_requires.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="guillaume.majeau-bettez@ntnu.no",
     long_description=open('README.md').read(),
     url='https://github.com/majeau-bettez/ecospold2matrix',
-    setup_requires=['numpy >= 1.11.0', 'lxml', 'nose >= 1.3.7',  'pandas >= 0.18.1', 'python-dateutil >= 2.5.3',
+    install_requires=['numpy >= 1.11.0', 'lxml', 'nose >= 1.3.7',  'pandas >= 0.18.1', 'python-dateutil >= 2.5.3',
                     'scipy >= 0.17.0', 'six >= 1.10.0', 'xlrd >= 0.9.4', 'xlwt >= 1.0.0'],
     include_package_data=True,
 )
